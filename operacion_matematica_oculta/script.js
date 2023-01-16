78d6f539254dd8974039ecdf1d3f66232f5ca71b
DAVID <dasamepu@gmail.com>
2023-01-15 19:02:39 -0500

    first commit

---
+++ operacion_matematica_oculta/script.js
@@ -0,0 +1,10 @@
+function solution(num1, num2) {
+  // Tu código aquí 👈
+  let res1 = num1 - num2
+  let res2 = num1 + num2
+  let result = res1 + "" + res2
+
+  return parseInt(result)
+}
+solution(5, 4)
+solution(21, 5)
